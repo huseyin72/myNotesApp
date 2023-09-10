c52398f1c57a698bc7e108c18003f97390688e7e
Huseyin Ozdemir <huseyinozdemir@huseyin-air.mynet>
2023-09-10 13:13:23 +0300

    app .js async function removed

--- app.js
+++ app.js
@@ -10,61 +10,59 @@ const path = require('path');
 const { checkUser } = require('./middleware/authMiddleware');
 
 
-async function startApp() {
-
-    await mongoose.connect(database.uri)
-        .then(() => {
-            console.log('mongoose ile bağlanıldı')
-
-        }).catch((err) => {
-            console.log(err);
-
-        });
-
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, 'views'));
 
 
+mongoose.connect(database.uri)
+    .then(() => {
+        console.log('mongoose ile bağlanıldı')
 
-    // createUser();
-    // async function createUser(){
-    //     try{
-    //         const user = await User.create({
-    //             username:'Huseyin',
-    //             email:'test@mail.com',
-    //             password:'test'
-    //         })
-    //         session_user = user;
-    //         console.log(user);
+    }).catch((err) => {
+        console.log(err);
 
-    //     }catch(err){
-    //         console.log(err);
+    });
 
-    //     }
-    // }
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 
-    //middleware--------------------------
 
-    //body parser
-    app.use(bodyParser.json());
+// createUser();
+// async function createUser(){
+//     try{
+//         const user = await User.create({
+//             username:'Huseyin',
+//             email:'test@mail.com',
+//             password:'test'
+//         })
+//         session_user = user;
+//         console.log(user);
 
-    //cookie
-    app.use(cookieParser());
+//     }catch(err){
+//         console.log(err);
 
-    //body parser
-    app.use(bodyParser.urlencoded({ extended: true }));
+//     }
+// }
 
 
+//middleware--------------------------
+
+//body parser
+app.use(bodyParser.json());
+
+//cookie
+app.use(cookieParser());
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
-    //app routes
-    app.use('*', checkUser);
-    app.use(routes)
 
 
+//app routes
+app.use('*', checkUser);
+app.use(routes)
 
 
 
@@ -73,19 +71,19 @@ async function startApp() {
 
 
 
-    // 404
-    app.use((req, res, next) => {
-        res.status(404).send("Sorry can't find that!")
-    })
+
+
+// 404
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
 
 
 
 
-    const port = 3000
-    app.listen(port, () => {
-        console.log('connected to port');
-    })
-}
+const port = 3000
+app.listen(port, () => {
+    console.log('connected to port');
+})
 
 
-startApp();
